49828740f5282a24e9f16b8406d72e09cdae74e8
itsRajanand <143922694+itsRajanand@users.noreply.github.com>
2024-06-19 10:49:48 +0000

    container config done

---
+++ 01_basics/02_dateType.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3)// we are using nodejs, not brower
+
+console.log(3
+    +
+    3);  //code readability should be high
+
+console.log("Raj Anand");
+
+let name = "Raj Anand"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "Raj Anand" );
+console.log(typeof null );
